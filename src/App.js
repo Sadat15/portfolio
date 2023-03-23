b87161551c86c3e4b950719f0f62793b9967af63
Sadat15 <sadatmanjur15@gmail.com>
2023-03-23 16:17:07 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import Navigation from "./components/Navigation";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Projects from "./pages/Projects.js";
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Route>
+    )
+  );
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Navigation />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import "bulma/css/bulma.css";
+
+function Home() {
+  return (
+    <div className="column is-three-fifths is-offset-one-fifth">
+      <div className="card">
+        <div className="title is-1 has-text-centered	">Sadat Manjur</div>
+        <div className="card-content">
+          I am a full-stack software developer with a passion for technology and
+          problem-solving. With a background in finance, I bring a unique
+          perspective to my work that combines analytical thinking with a
+          creative approach to problem-solving. Throughout my career, I have
+          developed a diverse skill set that allows me to tackle projects from
+          different angles and deliver high-quality results.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
